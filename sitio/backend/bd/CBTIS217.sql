c8928c1ab7fbf7da95f3b93f4c2eb8b6424b74c6
Juan Rafael Gonzalez Diaz <raf28elnooob@gmail.com>
2025-12-15 17:47:51 +0100

    Create database and update usuarios table structure
    Created a new database and modified the usuarios table structure, including changes to the email field and added user details.

--- sitio/backend/bd/CBTIS217.sql
+++ sitio/backend/bd/CBTIS217.sql
@@ -1,23 +1,32 @@
+CREATE DATABASE datos;
+USE datos;
+
 DROP TABLE IF EXISTS usuarios;
 
-CREATE TABLE usuarios (
+CREATE TABLE usuarios(
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(28) UNIQUE NOT NULL,
     pass VARCHAR(255) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE,
     fname VARCHAR(100) NOT NULL,
     rol ENUM('admin', 'user') DEFAULT 'user'
-);
+) ENGINE = InnoDB;
 
 
 
 INSERT INTO usuarios (username, pass, email, fname, rol) 
-VALUES ('DAV23', '$2y$10$uzy5urX26/cO89MUDqnzrOw4vmAOQ3Hp6HcCfmxv8M2AwCYx23MWO', 'david@cbtis217.edu.mx', 'Administrador', 'admin');
+VALUES (
+    'DAV23',
+    '$2y$10$uzy5urX26/cO89MUDqnzrOw4vmAOQ3Hp6HcCfmxv8M2AwCYx23MWO',
+    '',
+    'David Almanza Lopez',
+    'admin'
+);
 INSERT INTO usuarios (username, pass, email, fname, rol) 
 VALUES (
     'RAF28', 
     '$2y$10$uzy5urX26/cO89MUDqnzrOw4vmAOQ3Hp6HcCfmxv8M2AwCYx23MWO',
-    'RAF28@email.com', 
+    '', 
     'Juan Rafael Gonzalez Diaz', 
     'admin'
 );
