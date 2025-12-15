1e34461fc940ddc2be48cc54f297c149ddd987ce
david233421 <aald080507hgtlpva4@cbtis217.edu.mx>
2025-12-15 07:52:41 -0600

    Update logout.php

--- sitio/frontend/usuario/logout.php
+++ sitio/frontend/usuario/logout.php
@@ -1,13 +1,7 @@
 <?php
 session_start();
-
-// Destruir todas las variables de sesión
 $_SESSION = array();
-
-// Destruir la sesión
 session_destroy();
-
-// Redirigir al login
 header('Location: loging.php');
 exit();
 ?>
